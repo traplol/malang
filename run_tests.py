23d44887edb5fa0c5a03e49c2fb2ffa054ae24db
Max Mickey <max.mickey1991@gmail.com>
2017-11-19 12:35:13 -0600

    pretty colors.

--- run_tests.py
+++ run_tests.py
@@ -3,6 +3,7 @@
 import subprocess
 import glob
 import os
+import sys
 
 script_dir = os.path.dirname(os.path.realpath(__file__))
 os.chdir(script_dir)
@@ -13,6 +14,12 @@ def run_mal_with(args):
     res = subprocess.run(['./mal'] + args, stdout=subprocess.PIPE)
     return res.stdout
 
+def passed(filename):
+    sys.stdout.write("\033[1;32m PASS: {}\033[0;0m\n".format(filename))
+
+def failed(filename):
+    sys.stdout.write("\033[1;31m FAIL: {}\033[0;0m\n".format(filename))
+
 test_dir = 'examples/tests/'
 files = glob.glob(test_dir + "*.ma")
 for f in files:
@@ -22,4 +29,7 @@ for f in files:
         expected = exp.read()
     #print(actual)
     #print(expected)
-    print("{} : {}".format(expected == actual, f))
+    if expected == actual:
+        passed(f)
+    else:
+        failed(f)
